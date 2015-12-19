834152d2fca8d38778d23b6750a0abf02e3f9e41
shlomohass <shlomi@14tec.com>
2015-12-19 02:28:58 +0200

    New Lang control class - better definition of language tokens and naming system.

---
+++ Lang.cpp
@@ -0,0 +1,177 @@
+/* 
+ * File:   Lang.cpp
+ * Author: shlomi
+ * 
+ * Created on 10 דצמבר 2015, 17:15
+ */
+
+#include "Lang.h"
+
+//Initialize maps:
+map<char, string> Lang::LangInverseDelimiter = map<char, string>{};
+map<string, string> Lang::LangInverseKeywords = map<string, string>{};
+
+//Set Delimiters:
+map<string, char> Lang::LangDelimiter = {
+    {"space"             , ' '},
+    {"plus-cast-str"     , '~'},
+    {"plus"              , '+'},
+    {"minus"             , '-'},
+    {"multi"             , '*'},
+    {"divide"            , '/'},
+    {"equal"             , '='},
+    {"braketOpen"        , '('},
+    {"braketClose"       , ')'},
+    {"bracesOpen"        , '{'},
+    {"bracesClose"       , '}'},
+    {"power"             , '^'},
+    {"exclamation"       , '!'},
+    {"greater"           , '>'},
+    {"smaller"           , '<'},
+    {"comma"             , ','},
+    {"semicolon"         , ';'}
+};
+
+//Set Keywords:
+map<string, string> Lang::LangKeywords = {
+    {"variable"     , "var"},
+    {"cond-if"      , "if"},
+    {"cond-else"    , "else"},
+    {"loop-while"   , "while"},
+    {"loop-do"      , "do"},
+    {"return"       , "return"},
+};
+
+//Set chars allowed as names:
+//if we do not define '"' as a character, no tokens will be generated for string quoetation marks
+vector<char> Lang::LangNamingAllowedChars = {
+    'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+    'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+    '_' 
+};
+
+//Set allowed extensions:
+vector<string> Lang::extensionLib = {
+    "scs",
+    "SCS",
+    "owq",
+    "OWQ"
+};
+
+//Define constants definition characters:
+char Lang::LangStringIndicator          = '"';
+char Lang::LangFunctionOpenArguChar     = '(';
+char Lang::LangFunctionCloseArguChar    = ')';
+char Lang::LangBlockOpenChar            = '{';
+char Lang::LangBlockCloseChar           = '}';
+char Lang::LangArgumentSpacer           = ',';
+char Lang::LangStringEscape             = '\\';
+char Lang::LangOperationEnd             = ';';
+char Lang::LangMacroIndicator           = '#';
+char Lang::LangMacroSetChar             = ':';
+
+//Set Regex lib:
+map<string, string> Lang::LangRegexLib = {
+    {"remove-comments"     , "(/\\*([^*]|[\\r\\n]|(\\*+([^*/]|[\\r\\n])))*\\*+/)|(//.*)"}
+};   
+
+Lang::Lang() {
+    //Inverse Delimiter:    
+    for(auto const &ent1 : LangDelimiter) {
+        LangInverseDelimiter.insert(pair<char, string>(ent1.second, ent1.first));
+    } 
+    //Inverse Keywords:    
+    for(auto const &ent2 : LangKeywords) {
+        LangInverseKeywords.insert(pair<string, string>(ent2.second, ent2.first));
+    } 
+}
+Lang::Lang(const Lang& orig) {
+    
+}
+Lang::~Lang() {
+}
+/** Find the character that represent a delimiter name:
+ * 
+ * @param string key
+ * @return char
+ */
+char Lang::LangFindDelimiter(const string& key) {
+    if (LangHasKeyDelimiter(key)) {
+        return LangDelimiter.at(key);
+    }
+    return '\0';
+}
+/** Check whether a key delimiter is set:
+ * 
+ * @param string key key name
+ * @return bool
+ */
+bool Lang::LangHasKeyDelimiter(const string& key) {
+    return LangDelimiter.count(key) == 1;
+}
+/** Check whether a char delimiter is an actual register delimiter:
+ * 
+ * @param string key key name
+ * @return bool
+ */
+bool Lang::LangIsDelimiter(const char& value) {
+    return LangInverseDelimiter.count(value) == 1;
+}
+bool Lang::LangIsDelimiter(const string& value) {
+    return LangInverseDelimiter.count(value[0]) == 1;
+}
+/** Find the string that represent a keyword name (key):
+ * 
+ * @param string key
+ * @return string
+ */
+string Lang::LangFindKeyword(const string& key) {
+    if (LangHasKeyKeyword(key)) {
+        return LangKeywords.at(key);
+    }
+    return "";
+}
+/** Check whether a key keyword is set:
+ * 
+ * @param string key key name
+ * @return bool
+ */
+bool Lang::LangHasKeyKeyword(const string& key) {
+    return LangKeywords.count(key) == 1;
+}
+/** Check whether a string is an actual register keyword:
+ * 
+ * @param string key key name
+ * @return bool
+ */
+bool Lang::LangIsKeyword(const string& value) {
+    return LangInverseKeywords.count(value) == 1;
+}
+/** Checks whether a letter is allowed as function or variable name:
+ * 
+ * @param char|string[0] value
+ * @return bool
+ */
+bool Lang::LangIsNamingAllowed(const char& value) {
+    if (find(LangNamingAllowedChars.begin(), LangNamingAllowedChars.end(), value) != LangNamingAllowedChars.end()) {
+        return true;
+    }
+    return false;
+}
+bool Lang::LangIsNamingAllowed(const string& value) {
+    if (find(LangNamingAllowedChars.begin(), LangNamingAllowedChars.end(), value[0]) != LangNamingAllowedChars.end()) {
+        return true;
+    }
+    return false;
+}
+
+
+/*
+ * PRINTING STUFF:
+ */
+void Lang::printHeader(string headername) {
+    cout << endl << "-------------------------------------------------------------------" << endl;
+    cout << "| OWQ Debugger -> " << headername << endl;
+    cout << "-------------------------------------------------------------------" << endl;
+}
+
---
+++ Lang.h
@@ -0,0 +1,74 @@
+/* 
+ * File:   Lang.h
+ * Author: shlomi
+ *
+ * Created on 10 דצמבר 2015, 17:15
+ */
+
+#ifndef LANG_H
+#define	LANG_H
+
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+class Lang {
+public:
+    Lang();
+    Lang(const Lang& orig);
+    
+    //Regex lib:
+    static map<string, string>     LangRegexLib;
+    
+    //Allowed extensions:
+    static vector<string>          extensionLib;
+    
+    //Delimiters Definition and functions:
+    static map<string, char>       LangDelimiter;
+    static map<char, string>       LangInverseDelimiter;
+    static char                    LangFindDelimiter(const string& key);
+    static bool                    LangHasKeyDelimiter(const string& key);
+    static bool                    LangIsDelimiter(const char& value);
+    static bool                    LangIsDelimiter(const string& value);
+    
+    //Digits Definition and functions:
+    static map<int, char>          LangDigits;
+    
+    
+    //Keywords Definition and functions:
+    static map<string, string>     LangKeywords;
+    static map<string, string>     LangInverseKeywords;
+    static string                  LangFindKeyword(const string& key);
+    static bool                    LangHasKeyKeyword(const string& key);
+    static bool                    LangIsKeyword(const string& value);
+    
+    //Naming system Definition and functions:
+    static vector<char>            LangNamingAllowedChars;
+    static bool                    LangIsNamingAllowed(const string& value);
+    static bool                    LangIsNamingAllowed(const char& value);
+    
+    //Define constant definition characters:
+    static char                    LangStringIndicator;
+    static char                    LangFunctionOpenArguChar;
+    static char                    LangFunctionCloseArguChar;
+    static char                    LangBlockOpenChar;
+    static char                    LangBlockCloseChar;
+    static char                    LangArgumentSpacer;
+    static char                    LangStringEscape;
+    static char                    LangOperationEnd;
+    static char                    LangMacroIndicator;
+    static char                    LangMacroSetChar;
+    virtual ~Lang();
+    
+    //Printin stuff:
+    static void printHeader(string headername);
+private:
+
+};
+
+#endif	/* LANG_H */
+
